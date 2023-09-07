08c67172432bc9c3ca3b34e9f727300779f7c8b8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:40:00 +0300

    add AccessDeniedException

---
+++ src/main/java/by/aleksandrphilimonov/api/exceptions/AccessDeniedException.java
@@ -0,0 +1,10 @@
+package by.aleksandrphilimonov.api.exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
