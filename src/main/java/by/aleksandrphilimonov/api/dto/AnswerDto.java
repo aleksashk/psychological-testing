7d25157d4afb8aae8ca253f79ab82c8932a99a11
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:19:10 +0300

    add dto layer: AnswerDto

---
+++ src/main/java/by/aleksandrphilimonov/api/dto/AnswerDto.java
@@ -0,0 +1,18 @@
+package by.aleksandrphilimonov.api.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AnswerDto {
+
+    Integer id;
+
+    Short order;
+
+    String text;
+}
--- src/main/java/by/aleksandrphilimonov/api/exceptions/BadRequestException.java
+++ src/main/java/by/aleksandrphilimonov/api/exceptions/BadRequestException.java
@@ -1,4 +1,4 @@
-package by.aleksandrphilimonov.exceptions;
+package by.aleksandrphilimonov.api.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
