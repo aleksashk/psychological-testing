2673c8a5b8c37bde866992f04c96e777b7353900
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:02:05 +0300

    refactoring PsychologistRepository

--- src/main/java/by/aleksandrphilimonov/store/repositories/PsychologistRepository.java
+++ src/main/java/by/aleksandrphilimonov/store/repositories/PsychologistRepository.java
@@ -1,4 +1,11 @@
 package by.aleksandrphilimonov.store.repositories;
 
-public interface PsychologistRepository {
+import by.aleksandrphilimonov.store.entities.PsychologistEntity;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PsychologistRepository extends JpaRepository<PsychologistEntity, Integer> {
+    Optional<PsychologistEntity> findTopByLoginAndPassword(@NonNull String login, @NonNull String password);
 }
