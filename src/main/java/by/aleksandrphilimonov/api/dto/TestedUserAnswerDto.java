7c37c90723d7d0642759c72a1f5f604aa14debe9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:33:54 +0300

    add dto layer: TestedUserAnswerDto

---
+++ src/main/java/by/aleksandrphilimonov/api/dto/TestedUserAnswerDto.java
@@ -0,0 +1,20 @@
+package by.aleksandrphilimonov.api.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TestedUserAnswerDto {
+
+    Integer questionId;
+
+    Integer answerId;
+
+    Instant createdAt;
+}
