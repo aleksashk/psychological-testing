1de5e0cd35e824792a8144473e4591c3c1abc51e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:34:47 +0300

    add dto layer: TestDto

---
+++ src/main/java/by/aleksandrphilimonov/api/dto/TestDto.java
@@ -0,0 +1,20 @@
+package by.aleksandrphilimonov.api.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TestDto extends LiteTestDto {
+
+    List<QuestionDto> questions;
+
+    List<PersonTemplateDto> personTemplates;
+}
