09de281b5104d8387b03eb5e98d7d413a1c10989
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 13:56:29 +0300

    add entity: SchoolEntity

--- src/main/java/by/aleksandrphilimonov/store/entities/SchoolEntity.java
+++ src/main/java/by/aleksandrphilimonov/store/entities/SchoolEntity.java
@@ -1,4 +1,39 @@
 package by.aleksandrphilimonov.store.entities;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "school")
 public class SchoolEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Integer id;
+
+    @Column(length = 128)
+    @NonNull
+    String name;
+
+    @Builder.Default
+    @OneToMany
+    @JoinColumn(name = "school_id", referencedColumnName = "id")
+    List<SchoolClassEntity> schoolClasses = new ArrayList<>();
+
+    public static SchoolEntity makeDefault(String schoolName) {
+
+        return builder()
+                .name(schoolName)
+                .build();
+    }
 }
