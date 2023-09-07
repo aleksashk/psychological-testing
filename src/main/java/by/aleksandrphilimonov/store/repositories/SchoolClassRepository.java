83a0b389bc77c7e14d0b2f46acc1cdeb4633a616
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:04:04 +0300

    refactoring SchoolClassRepository

--- src/main/java/by/aleksandrphilimonov/store/repositories/SchoolClassRepository.java
+++ src/main/java/by/aleksandrphilimonov/store/repositories/SchoolClassRepository.java
@@ -1,4 +1,10 @@
 package by.aleksandrphilimonov.store.repositories;
 
-public interface SchoolClassRepository {
+import by.aleksandrphilimonov.store.entities.SchoolClassEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SchoolClassRepository extends JpaRepository<SchoolClassEntity, Integer> {
+    Optional<SchoolClassEntity> findByName(String name);
 }
