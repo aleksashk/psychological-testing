0bec183bd0fe08d82eb5f15b88b7a111a214208f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 13:40:15 +0300

    add entities: Answer, Question, Test

--- src/main/java/by/aleksandrphilimonov/store/entities/QuestionEntity.java
+++ src/main/java/by/aleksandrphilimonov/store/entities/QuestionEntity.java
@@ -1,4 +1,44 @@
 package by.aleksandrphilimonov.store.entities;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "question")
 public class QuestionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Integer id;
+
+    Short questionOrder;
+
+    @Column(length = 256)
+    String text;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id", referencedColumnName = "id")
+    TestEntity test;
+
+    @Column(name = "test_id", updatable = false, insertable = false)
+    Long testId;
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", referencedColumnName = "id")
+    List<AnswerEntity> answers = new ArrayList<>();
+
+    public static QuestionEntity makeDefault() {
+        return builder().build();
+    }
 }
--- src/main/java/by/aleksandrphilimonov/store/entities/TestEntity.java
+++ src/main/java/by/aleksandrphilimonov/store/entities/TestEntity.java
@@ -1,4 +1,52 @@
 package by.aleksandrphilimonov.store.entities;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "test")
 public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Integer id;
+
+    @Column(length = 256)
+    String name;
+
+    @Builder.Default
+    Boolean status = false;
+
+    @ManyToOne
+    @JoinColumn(name = "psychologist_id", referencedColumnName = "id")
+    PsychologistEntity psychologist;
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id", referencedColumnName = "id")
+    List<PersonTemplateEntity> personTemplates = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id", referencedColumnName = "id")
+    List<TestAnswerEntity> testAnswers = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id", referencedColumnName = "id")
+    List<QuestionEntity> questions = new ArrayList<>();
+
+    public static TestEntity makeDefault() {
+        return builder().build();
+    }
 }
