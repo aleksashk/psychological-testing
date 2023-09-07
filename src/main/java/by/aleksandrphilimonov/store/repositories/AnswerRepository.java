9b584bdaf17634e48ced4c4b0695df7d50f7cc51
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:00:52 +0300

    refactoring AnswerRepository

--- src/main/java/by/aleksandrphilimonov/store/repositories/AnswerRepository.java
+++ src/main/java/by/aleksandrphilimonov/store/repositories/AnswerRepository.java
@@ -1,4 +1,7 @@
 package by.aleksandrphilimonov.store.repositories;
 
-public interface AnswerRepository {
+import by.aleksandrphilimonov.store.entities.AnswerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<AnswerEntity, Integer> {
 }
