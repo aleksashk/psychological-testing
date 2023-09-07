32dd7a2e059041e45730aa3c48cfe01fcad89e77
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-07 14:38:03 +0300

    add dto layer: LiteTestDto

---
+++ src/main/java/by/aleksandrphilimonov/api/dto/LiteTestDto.java
@@ -0,0 +1,20 @@
+package by.aleksandrphilimonov.api.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LiteTestDto {
+
+    Integer id;
+
+    String name;
+}
